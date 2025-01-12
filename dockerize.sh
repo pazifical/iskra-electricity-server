7def3540323d912e464dc8ad530322117f13b527
Paz <paz@twowaysix.com>
2025-01-12 12:13:33 +0100

    changed workflow

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,3 @@
 source version.env
 
-# docker build -t iskra-electricity-server:$VERSION .
-
-docker buildx build --push --platform linux/arm64,linux/amd64 --tag "pazifical/iskra-electricity-server:$VERSION" .
+docker build -t pazifical/iskra-electricity-server:$VERSION .
